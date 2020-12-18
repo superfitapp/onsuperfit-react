a8f8d22dc7f38410b9611223c9991a23d0780d1e
Leo Kwan <leo@superfitapp.com>
2020-12-18 18:10:46 -0500

    add url scheme

--- lib/auth0.tsx
+++ lib/auth0.tsx
@@ -6,9 +6,9 @@ export default initAuth0({
   scope: process.env.NEXT_PUBLIC_AUTH0_SCOPE || 'openid profile',
   domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
   redirectUri:
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/callback`,
+    `${process.env.NEXT_PUBLIC_URL_SCHEME}${process.env.NEXT_PUBLIC_BASE_URL}/api/callback`,
   postLogoutRedirectUri:
-    `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+    `${process.env.NEXT_PUBLIC_URL_SCHEME}${process.env.NEXT_PUBLIC_BASE_URL}/`,
   session: {
     cookieSecret: process.env.SESSION_COOKIE_SECRET,
     cookieLifetime: Number(process.env.SESSION_COOKIE_LIFETIME) || 7200,
